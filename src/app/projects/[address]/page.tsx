60ed31fdc1ce3a1776e176c0c9217976eaedc9cc
sarvesh-karki <sarvesh.karki@rumsan.net>
2023-07-07 16:22:38 +0545

    project update

--- src/app/projects/[address]/page.tsx
+++ src/app/projects/[address]/page.tsx
@@ -1,1 +1,13 @@
 // details page
+// sections
+import { ProjectDetailsView } from 'src/sections/project/view';
+
+// ----------------------------------------------------------------------
+
+export const metadata = {
+    title: 'Project: Details',
+};
+
+export default function ProductShopDetailsPage() {
+    return <ProjectDetailsView />;
+}
