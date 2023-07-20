a0f1ea8e5f6ba4a54fe06e57921215af7e2b3b94
Raghav Kattel <arg.ahv7@gmail.com>
2023-07-20 10:11:21 +0545

    project card

--- src/sections/project/project-card-item.tsx
+++ src/sections/project/project-card-item.tsx
@@ -5,34 +5,32 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { IProjectItem } from 'src/types/project';
 
-type ProjectCard = {
-    card: IProjectItem
-}
+type IProjectCard = {
+  card: IProjectItem;
+};
 
-export default function ProjectCard({ card }: ProjectCard) {
-    return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-                sx={{ height: 240 }}
-                image={card.image}
-                title="green iguana"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h6" component="div">
-                    {card.title}
-                </Typography>
-                <Box sx={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                }}>
-                    <Typography variant="body2" gutterBottom>
-                        {card.createdAt}
-                    </Typography>
-                    <Typography variant="body2" gutterBottom>
-                        ${card.budget}
-                    </Typography>
-                </Box>
-            </CardContent>
-        </Card>
-    );
+export default function ProjectCard({ card }: IProjectCard) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia sx={{ height: 240 }} image={card.image} title="green iguana" />
+      <CardContent>
+        <Typography gutterBottom variant="h6" component="div">
+          {card.title}
+        </Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Typography variant="body2" gutterBottom>
+            {card.createdAt}
+          </Typography>
+          <Typography variant="body2" gutterBottom>
+            ${card.budget}
+          </Typography>
+        </Box>
+      </CardContent>
+    </Card>
+  );
 }
--- src/sections/project/view/project-actions-card.tsx
+++ src/sections/project/view/project-actions-card.tsx
@@ -1,20 +1,18 @@
-import { Button, Card, CardContent, Stack } from "@mui/material"
+import { Button, Card, CardContent, Stack } from '@mui/material';
 
-const ProjectActions = () => {
-    return (
-        <Card sx={{ mb: 2 }}>
-            <CardContent>
-                <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
-                    <Button variant="outlined" color="success">
-                        Beneficiary List
-                    </Button>
-                    <Button variant="outlined" color="primary">
-                        Actions
-                    </Button>
-                </Stack>
-            </CardContent>
-        </Card>
-    )
-}
+const ProjectActions = () => (
+    <Card sx={{ mb: 2 }}>
+      <CardContent>
+        <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
+          <Button variant="outlined" color="success">
+            Beneficiary List
+          </Button>
+          <Button variant="outlined" color="primary">
+            Actions
+          </Button>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
 
-export default ProjectActions
+export default ProjectActions;
