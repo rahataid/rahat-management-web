9ccd8201199560fa16c9576ecde2802c485dd980
prithvi-rumsan <maharjanprithvi@gmail.com>
2023-07-21 14:00:15 +0545

    Vendors details page vendor card made dynamic

--- src/sections/vendors/view/VendorView.tsx
+++ src/sections/vendors/view/VendorView.tsx
@@ -2,13 +2,16 @@ import CustomBreadcrumbs from '@components/custom-breadcrumbs/custom-breadcrumbs
 import { useSettingsContext } from '@components/settings';
 import { Container, Grid, Stack } from '@mui/material';
 import { paths } from '@routes/paths';
-
+import { useVendor } from 'src/api/vendors';
+import { useParams } from 'src/routes/hook';
 import BasicInfoCard from './basic-info-card';
 import VendorsCards from './transaction-info-card';
 import TransactionTable from './transaction-list-card';
 
 const VendorView = () => {
   const settings = useSettingsContext();
+  const { address } = useParams();
+  const { vendor } = useVendor(address);
 
   return (
     <Container maxWidth={settings.themeStretch ? false : 'lg'}>
@@ -25,7 +28,7 @@ const VendorView = () => {
             <VendorsCards />
           </Grid>
           <Grid item xs={12} md={4}>
-            <BasicInfoCard />
+            <BasicInfoCard data={vendor} />
           </Grid>
         </Grid>
         <Grid container spacing={3}>
@@ -33,7 +36,7 @@ const VendorView = () => {
             <TransactionTable />
           </Grid>
           <Grid item xs={12} md={6}>
-            <BasicInfoCard />
+            <BasicInfoCard data={vendor} />
           </Grid>
         </Grid>
       </Stack>
